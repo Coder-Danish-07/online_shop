ddbed351362d442b850eb026d95018cdc5473025
Danish Khan <danishkhan11506@gmail.com>
2024-09-07 15:04:50 +0530

    Update cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Contracts\Session\Session;
 
 class CartController extends Controller
 {
@@ -39,7 +40,8 @@ class CartController extends Controller
             Cart::add($product->id, $product->title, 1, $product->price,['productImage' => (!empty($product->product_images)) ? $product->product_images->first() : '']);
 
             $status = true;
-            $message = $product->title.' added in cart';
+            $message = '<strong>'.$product->title.'</strong> added in your cart successfully';
+            Session()->flash('success',$message);
 
             }else{
                 $status = false;
@@ -50,7 +52,8 @@ class CartController extends Controller
             // empty in cart 
             Cart::add($product->id, $product->title, 1, $product->price,['productImage' => (!empty($product->product_images)) ? $product->product_images->first() : '']);
             $status = true;
-            $message = $product->title.' added in cart';
+            $message = '<strong>'.$product->title.'</strong> added in your cart successfully';
+            Session()->flash('success',$message);
         }
 
         return response()->json([
@@ -66,4 +69,61 @@ class CartController extends Controller
         $data['cartContent'] = $cartContent;
         return view('front.cart', $data);
     }
+
+    public function updateCart(Request $request){
+        $rowId = $request->rowId;
+        $Qty = $request->Qty;
+
+        //trackQty
+        $itemInfo = Cart::get($rowId);
+        $product = Product::find($itemInfo->id);
+        if($product->track_qty == "Yes"){
+            if($Qty <= $product->qty){
+                Cart::update( $rowId, $Qty );
+                $message = 'Cart Updated Successfully';
+                $status = true;
+                session()->flash('success', $message);
+            }
+            else{
+                $message = 'Requested qty('.$Qty.') not available in stock';
+                $status = false;
+                session()->flash('error', $message);
+            }
+        }
+        else{
+            Cart::update( $rowId, $Qty );
+            $message = 'Cart Updated Successfully';
+            $status = true;
+            session()->flash('success', $message);
+        }
+        return response()->json([
+            'status' => $status,
+            'message' => $message
+        ]);
+
+    }
+
+    public function deleteItem(Request $request){
+
+        $itemInfo = Cart::get($request->rowId);
+
+        if($itemInfo == null){
+
+            Session()->flash('error','Item not found');
+            return response()->json([
+                'status'=> false,
+                'message'=> 'Item not found'
+            ]);
+        }
+
+        Cart::remove($request->rowId);
+        Session()->flash('success','Item Deleted Successfully');
+        return response()->json([
+            'status'=> true,
+            'message'=> 'Item Deleted Successfully'
+            ]);
+
+    }
+
+    
 }
--- resources/views/front/cart.blade.php
+++ resources/views/front/cart.blade.php
@@ -16,6 +16,23 @@
 <section class=" section-9 pt-4">
     <div class="container">
         <div class="row">
+            @if(Session::has('success'))
+            <div class="col-md-12">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {!! Session::get('success') !!}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+            @endif
+            @if(Session::has('error'))
+            <div class="col-md-12">
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ Session::get('error') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+            @endif
+            @if(Cart::count() > 0)
             <div class="col-md-8">
                 <div class="table-responsive">
                     <table class="table" id="cart">
@@ -29,10 +46,10 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @if(!empty($cartContent))
+                            {{-- @if(!empty($cartContent)) --}}
                             @foreach($cartContent as $item)
                             <tr>
-                                <td>
+                                <td class="text-start">
                                     <div class="d-flex align-items-center">
                                         {{-- <img src="images/product-1.jpg" width="" height=""> --}}
                                         @if(!empty($item->options->productImage->image))
@@ -47,13 +64,13 @@
                                 <td>
                                     <div class="input-group quantity mx-auto" style="width: 100px;">
                                         <div class="input-group-btn">
-                                            <button class="btn btn-sm btn-dark btn-minus p-2 pt-1 pb-1">
+                                            <button class="btn btn-sm btn-dark btn-minus p-2 pt-1 pb-1 sub" data-id="{{ $item->rowId}}">
                                             <i class="fa fa-minus"></i>
                                             </button>
                                         </div>
                                         <input type="text" class="form-control form-control-sm  border-0 text-center" value="{{$item->qty}}">
                                         <div class="input-group-btn">
-                                            <button class="btn btn-sm btn-dark btn-plus p-2 pt-1 pb-1">
+                                            <button class="btn btn-sm btn-dark btn-plus p-2 pt-1 pb-1 add" data-id="{{$item->rowId}}">
                                                 <i class="fa fa-plus"></i>
                                             </button>
                                         </div>
@@ -63,11 +80,11 @@
                                     ${{$item->price*$item->qty}}
                                 </td>
                                 <td>
-                                    <button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button>
+                                    <button class="btn btn-sm btn-danger" onclick="deleteItem('{{$item->rowId}}');"><i class="fa fa-times"></i></button>
                                 </td>
                             </tr>
                             @endforeach 
-                            @endif                               
+                            {{-- @endif                                --}}
                         </tbody>
                     </table>
                 </div>
@@ -100,7 +117,71 @@
                     <button class="btn btn-dark" type="button" id="button-addon2">Apply Coupon</button>
                 </div>  --}}
             </div>
+            @else
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body d-flex justify-content-center align-items-center">
+                        <h4>Your Cart Is Empty!</h4>
+                    </div>
+                </div>
+            </div>
+            @endif                               
         </div>
     </div>
 </section>
+@endsection
+@section('customJs')
+<script>
+$('.add').click(function(){
+    var qtyElement = $(this).parent().prev(); // Qty Input
+    var qtyValue = parseInt(qtyElement.val());
+    if (qtyValue < 10) {
+        qtyElement.val(qtyValue+1);
+        var newQty = qtyElement.val();
+        var rowId = $(this).data('id');
+        updateCart(rowId,newQty);
+    }            
+});
+
+$('.sub').click(function(){
+    var qtyElement = $(this).parent().next(); 
+    var qtyValue = parseInt(qtyElement.val());
+    if (qtyValue > 1) {
+        qtyElement.val(qtyValue-1);
+        var newQty = qtyElement.val();
+        var rowId = $(this).data('id');
+        updateCart(rowId,newQty);
+    }        
+});
+
+function updateCart(rowId,Qty){
+    $.ajax({
+        url : "{{ route('front.updateCart')}}",
+        type: "post",
+        data: {rowId:rowId,Qty:Qty},
+        dataType: "json",
+        success:function(response){
+            // if(response.status == true){
+                window.location.href = "{{ route('front.cart')}}";
+            // }
+        }
+    });
+}
+
+function deleteItem(rowId){
+    if(confirm("Are You sure a want to delete a cart")){
+        $.ajax({
+        url : "{{ route('front.deleteItem.cart')}}",
+        type: "post",
+        data: {rowId:rowId},
+        dataType: "json",
+        success:function(response){
+            // if(response.status == true){
+                window.location.href = "{{ route('front.cart')}}";
+            // }
+        }
+    });
+    }
+}
+</script>
 @endsection
