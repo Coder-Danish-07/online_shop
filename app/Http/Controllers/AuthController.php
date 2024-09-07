894190ce3b6528b536e4448a863f7e5c515814f0
Danish Khan <danishkhan11506@gmail.com>
2024-09-07 19:58:27 +0530

    login autienticate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
 class AuthController extends Controller
 {
     public function login(){
@@ -47,5 +49,46 @@ class AuthController extends Controller
                 'errors' => $validaor->errors()
             ]);
         }
+    }
+
+    public function authienticate(Request $request){
+
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if($validator->passes()){
+
+            if(Auth::attempt(['email'=> $request->email,'password'=> $request->password],$request->get('remember'))){
+
+                return redirect()->route("account.profile");
+
+            }
+            else{
+                return redirect()->route("account.login")
+                ->withInput($request->only('email'))
+                ->with('error', 'Either Email/Password is incorrect');
+            }
+
+        }
+        else{
+
+            return redirect()->route("account.login")
+            ->withErrors($validator)
+            ->withInput($request->only('email'));
+
+        }
+
+    }
+
+    public function profile(){
+        return view('front.account.profile');
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('account.login')
+        ->with('success', 'You successfully logged out');
     }
 }
